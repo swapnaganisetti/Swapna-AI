8d981a0e3a407c24bbcea82b02fdddb31fd29080
swapnaganisetti <168207743+swapnaganisetti@users.noreply.github.com>
2025-04-08 16:19:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import BlogList from './components/BlogList';
+import BlogDetail from './components/BlogDetail';
+import CreateBlog from './components/CreateBlog';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-100">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/blogs" element={<BlogList />} />
+              <Route path="/blogs/:id" element={<BlogDetail />} />
+              <Route
+                path="/blogs/create"
+                element={
+                  <ProtectedRoute>
+                    <CreateBlog />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
